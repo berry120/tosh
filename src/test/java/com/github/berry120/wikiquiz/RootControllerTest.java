fdc8b08e66c75ad67cfcf6f010c4278d0d7ee76c
berry120 <berry120@gmail.com>
2020-07-08 00:53:31 +0100

    Remove redundant test

--- src/test/java/com/github/berry120/wikiquiz/RootControllerTest.java
+++ src/test/java/com/github/berry120/wikiquiz/RootControllerTest.java
@@ -1,21 +1,9 @@
 package com.github.berry120.wikiquiz;
 
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class RootControllerTest {
 
-    @Test
-    public void testHelloEndpoint() {
-        given()
-                .when().get("/hello")
-                .then()
-                .statusCode(200)
-                .body(is("hello"));
-    }
 
 }
